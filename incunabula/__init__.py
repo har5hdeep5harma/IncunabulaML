62785fb0fbdf5bfae1bd1b1b816f9ab2da6f076a
Harshdeep Sharma <harsh7251909511@gmail.com>
2025-08-16 14:58:09 +0530

    Update __init__.py

--- incunabula/__init__.py
+++ incunabula/__init__.py
@@ -2,11 +2,16 @@
 IncunabulaML
 ------------
 
-A lightweight educational package with foundational machine learning algorithms implemented from scratch.
+A lightweight educational package with foundational machine learning
+algorithms implemented from scratch.
 """
 
 from .perceptron import Perceptron
+from .adaline_gd import AdalineGD
+from .adaline_sgd import AdalineSGD
 
 __all__ = [
     "Perceptron",
+    "AdalineGD",
+    "AdalineSGD",
 ]
