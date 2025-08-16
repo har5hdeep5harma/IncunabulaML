8c3440367e7550c1915705617e24c19bdba35a0e
Harshdeep Sharma <harsh7251909511@gmail.com>
2025-08-16 14:08:29 +0530

    Create __init__.py

---
+++ incunabula/__init__.py
@@ -0,0 +1,12 @@
+"""
+IncunabulaML
+------------
+
+A lightweight educational package with foundational machine learning algorithms implemented from scratch.
+"""
+
+from .perceptron import Perceptron
+
+__all__ = [
+    "Perceptron",
+]
