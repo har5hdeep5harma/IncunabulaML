aab8936c5f3d1d6253ff85a333ef492c0d61df2f
Harshdeep Sharma <harsh7251909511@gmail.com>
2025-08-16 14:13:25 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="IncunabulaML",
+    version="0.1.0",
+    description="Foundational machine learning algorithms implemented from scratch in Python.",
+    author="Harshdeep Sharma",
+    author_email="harsh7251909511@gmail.com", 
+    url="https://github.com/har5hdeep5harma/IncunabulaML", 
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+    ],
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: MIT License",
+        "Intended Audience :: Education",
+        "Topic :: Artificial Intelligence",
+    ],
+)
