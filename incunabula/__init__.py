f5fa27a3512b49f8b5f6930cbf282ee49e230b7b
Harshdeep Sharma <harsh7251909511@gmail.com>
2025-08-17 14:35:13 +0530

    Update __init__.py

--- incunabula/__init__.py
+++ incunabula/__init__.py
@@ -8,6 +8,7 @@ A lightweight educational package with foundational machine learning algorithms 
 from .perceptron import Perceptron
 from .adaline_gd import AdalineGD
 from .adaline_sgd import AdalineSGD
+from .logistic_regression_gd import LogisticRegressionGD
 
 __all__ = [
     "Perceptron",
