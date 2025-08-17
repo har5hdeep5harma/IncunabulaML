3a5ef8808909c8a744243d4ebd3d6b258e0810cc
Harshdeep Sharma <harsh7251909511@gmail.com>
2025-08-18 00:18:57 +0530

    Create rbf_kernel_pca_basic.py

---
+++ incunabula/rbf_kernel_pca_basic.py
@@ -0,0 +1,42 @@
+import numpy as np
+from scipy.spatial.distance import pdist, squareform
+from scipy.linalg import eigh
+
+def rbf_kernel_pca_basic(X, gamma, n_components):
+    """
+    Basic RBF Kernel PCA.
+
+    Parameters
+    ----------
+    X : ndarray, shape = [n_samples, n_features]
+        Input data.
+    gamma : float
+        RBF kernel parameter.
+    n_components : int
+        Number of principal components to return.
+
+    Returns
+    -------
+    X_proj : ndarray, shape = [n_samples, n_components]
+        Projected dataset.
+    """
+    # Step 1: Pairwise squared Euclidean distances
+    sq_dists = pdist(X, 'sqeuclidean')
+    mat_sq_dists = squareform(sq_dists)
+
+    # Step 2: Kernel matrix
+    K = np.exp(-gamma * mat_sq_dists)
+
+    # Step 3: Center kernel matrix
+    N = K.shape[0]
+    one_n = np.ones((N, N)) / N
+    K_centered = K - one_n @ K - K @ one_n + one_n @ K @ one_n
+
+    # Step 4: Eigen decomposition
+    eigvals, eigvecs = eigh(K_centered)
+    eigvals, eigvecs = eigvals[::-1], eigvecs[:, ::-1]
+
+    # Step 5: Collect top k eigenvectors
+    X_proj = np.column_stack([eigvecs[:, i] for i in range(n_components)])
+
+    return X_proj
